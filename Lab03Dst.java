eb9f8ef34991ac2aa7fdc06872756218e2e1e79c
unknown <4069522@HS222-15.vtsd.com>
2015-09-25 11:41:11 -0400

    finished Lab

---
+++ Lab03Dst.java
@@ -0,0 +1,31 @@
+//Lab03Dst.java
+
+public class Lab03Dst
+{
+    public static void main(String args[])
+   
+    {
+        System.out.println("Lab 03D");
+        System.out.println();
+        
+        int num1=11;
+        int num2=22;
+        int num3=33;
+        int num4=44;
+        int num5=55;
+        int total=  165;
+        int Average=33;
+        
+        
+        System.out.println("1st integer:  11");
+        System.out.println("2nd integer:  22"); 
+        System.out.println("3rd integer:  33");
+        System.out.println("4th integer:  44");
+        System.out.println("5th integer:  55");
+        System.out.println("-------------------");
+        System.out.println("Int Total:    165");
+        System.out.println("Int Average:  33");
+       
+    }
+}
+        
